bbadf49af259d28aac8ba737ab5fcc0f23efa77b
Srini K <srinikdevops0131@gmail.com>
2023-03-28 14:52:44 -0700

    updating ./tf-module-rabbitmq

--- data.tf
+++ data.tf
@@ -3,3 +3,9 @@ data "aws_ami" "ami" {
   name_regex  = "devops-practice-with-ansible"
   owners      = ["self"]
 }
+
+#data "aws_route53_zone" "domain" {
+#  name = var.dns_domain
+#}
+#
+#data "aws_caller_identity" "account" {}

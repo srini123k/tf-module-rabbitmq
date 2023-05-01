fc5624eff89ba6a750432de2ea9229bb1ec48ecc
Srini K <srinikdevops0131@gmail.com>
2023-04-30 22:06:21 -0700

    updating ./tf-module-rabbitmq

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "devops-practice-with-ansible"
+#  owners      = ["self"]
+#}
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = ["self"]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["922057391995"]
 }
 
 data "aws_route53_zone" "domain" {
--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/srini123k/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
